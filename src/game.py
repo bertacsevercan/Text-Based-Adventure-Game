c21d105f0e17985c653cfc58472d635ffc47765a
bertacsevercan <bertac.severcan@gmail.com>
2020-12-29 12:43:51 +0300

    add welcome func and menu class

--- src/game.py
+++ src/game.py
@@ -1,1 +1,29 @@
+def welcome():
+    print(Menu.message + "\n" + Menu.options, end="=> ")
 
+
+class Menu:
+    title = "Journey to Mount Qaf"
+    message = f"***Welcome to the {title}***"
+    options = """
+-Press key '1' to start a new game
+-Press key '2' to load your progress
+-Press key '3' to quit the game
+
+"""
+
+    def __init__(self, user_input):
+        self.user_input = user_input
+
+    def new_game(self):
+        pass
+
+    def load_game(self):
+        pass
+
+    def quit(self):
+        pass
+
+
+welcome()
+game_menu = Menu(input())
