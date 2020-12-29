aab087117459ab07bfba649dbb076b2d93c62954
bertacsevercan <bertac.severcan@gmail.com>
2020-12-29 23:45:45 +0300

    add game state class

--- src/game.py
+++ src/game.py
@@ -1,8 +1,5 @@
 from sys import exit  # s1
 
-char_att_dict = {}  # s2
-inventory_dict = {}  # s2
-
 
 def welcome():  # s1
     print(Menu.message + "\n" + Menu.options, end="=> ")
@@ -12,25 +9,30 @@ def warning_unknown_input():  # s1
     print("Unknown input! Please enter a valid one.")
 
 
-class NewGame:  # s2
-    char_att_list = ["1- Name => ", "2- Species => ", "3- Gender => "]
-    inventory_list = ["1- Favourite Snack => ", "2- A weapon for the journey => ", "3- A traversal tool => "]
-
-    save_file = None
+class Game:  # s2
+    char_att_dict = {}  # s2
+    inventory_dict = {}  # s2
     difficulty = "Medium"
     lives = 3
+    level = 1
 
-    def __init__(self, user_input):
-        self.user_input = user_input
+
+class NewGame(Game):  # s2
+    char_att_list = ["1- Name => ", "2- Species => ", "3- Gender => "]
+    inventory_list = ["1- Favourite Snack => ", "2- A weapon for the journey => ", "3- A traversal tool => "]
+    save_file = None
+
+    def __init__(self, username_input):
+        self.username_input = username_input
 
     def create_new_save(self):
-        self.save_file = open(f"./gameSaves/{self.user_input}.txt", "w", encoding="utf-8")
+        self.save_file = open(f"./gameSaves/{self.username_input}.txt", "w", encoding="utf-8")
 
     def create_char(self, user_inputs):
         self.save_file.write(user_inputs + "\n")
 
     def create_new_game(self):  # s2
-        if self.user_input == "/quit":
+        if self.username_input == "/quit":
             exit()
         else:
             self.create_new_save()
@@ -39,14 +41,14 @@ class NewGame:  # s2
             for i in range(len(self.char_att_list)):
                 user_input = input(self.char_att_list[i])
                 self.create_char(user_input)  # save the inputs to a file
-                char_att_dict[char_dict_keys[i]] = user_input  # save the inputs to a dict
+                self.char_att_dict[char_dict_keys[i]] = user_input  # save the inputs to a dict
 
             print("Pack your bag for the journey:")
             inventory_dict_keys = ["snack", "weapon", "tool"]
             for j in range(len(self.inventory_list)):
                 user_input = input(self.inventory_list[j])
                 self.create_char(user_input)
-                inventory_dict[inventory_dict_keys[j]] = user_input
+                self.inventory_dict[inventory_dict_keys[j]] = user_input
 
             difficulty_input = input("""
 Choose your difficulty:
@@ -71,10 +73,7 @@ Choose your difficulty:
             self.save_file.close()
 
 
-class GameFunctions:  # s2
-
-    def __init__(self, lives):
-        self.lives = lives
+class GameFunctions(Game):  # s2
 
     def increase_lives(self):
         self.lives += 1
@@ -83,7 +82,7 @@ class GameFunctions:  # s2
         self.lives -= 1
 
     def show_inventory(self):
-        inventory = list(inventory_dict.values())
+        inventory = list(self.inventory_dict.values())
         print(f"Inventory: {inventory}")
 
 
@@ -94,7 +93,6 @@ class Menu:  # s1, in the s1 ,the functions should be passed, they are implemete
 1- Press key '1' or type 'start' to start a new game
 2- Press key '2' or type 'load to load your progress
 3- Press key '3' or type 'quit' to quit the game
-
 """
 
     def __init__(self, user_input):
@@ -102,7 +100,7 @@ class Menu:  # s1, in the s1 ,the functions should be passed, they are implemete
 
     def new_game(self):
         print("Starting a new game...")  # s1
-        message = "Enter a user name to save your progress or type '/quit' to quit the game => \n"  # s2
+        message = "Enter a user name to save your progress or type '/quit' to quit the game => "  # s2
         new_game = NewGame(input(message))
         new_game.create_new_game()
 
@@ -113,10 +111,10 @@ class Menu:  # s1, in the s1 ,the functions should be passed, they are implemete
 # s1
 welcome()
 game_menu = Menu(input())
+game_state = Game()
 
 if game_menu.user_input == "1" or game_menu.user_input.lower() == "start":
     game_menu.new_game()
-
 elif game_menu.user_input == "2" or game_menu.user_input.lower() == "load":
     game_menu.load_game()
 elif game_menu.user_input == "3" or game_menu.user_input.lower() == "quit":
