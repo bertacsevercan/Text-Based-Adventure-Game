58cad48c0fd0782fd26f0a27b7130d91ac014e9b
bertacsevercan <bertac.severcan@gmail.com>
2021-01-10 00:40:11 +0300

    add test for stage 1

--- Text-Based-Adventure-Game/stage1/tests.py
+++ Text-Based-Adventure-Game/stage1/tests.py
@@ -1,14 +1,50 @@
-from hstest.stage_test import StageTest
-from hstest.test_case import TestCase
+from typing import Any
+from hstest.stage_test import StageTest, TestPassed, WrongAnswer
+from hstest.test_case import TestCase, SimpleTestCase
 from hstest.check_result import CheckResult
 
 
 class TextBasedAdventureGameTest(StageTest):
-    def generate(self):
-        return [TestCase()]
+    def generate(self) -> [TestCase]:
+        return [
+            TestCase(stdin=[self.check_welcome]),
+            TestCase(stdin=["1", self.check_start_load]),
+            TestCase(stdin=["start", self.check_start_load]),
+            TestCase(stdin=["StARt", self.check_start_load]),
+            TestCase(stdin=["2", self.check_start_load]),
+            TestCase(stdin=["load", self.check_start_load]),
+            TestCase(stdin=["lOAd", self.check_start_load]),
+            TestCase(stdin=["4", self.check_unknown]),
+            TestCase(stdin="3"),
+            TestCase(stdin="quIt")
+        ]
 
-    def check(self, reply, attach):
+    def check_welcome(self, output):
+        if "Welcome to" not in output and "***" not in output:
+            return CheckResult.wrong("You didn't output a correct welcome message!")
         return CheckResult.correct()
+
+    def check_start_load(self, output):
+        if "Starting a new game..." in output or "Loading your progress..." in output:
+            return CheckResult.correct()
+        return CheckResult.wrong("Your program didn't output correct message.")
+
+    def check_exit(self, output):
+        if "Goodbye!" in output:
+            raise TestPassed()
+        else:
+            raise WrongAnswer("Your program didn't print a goodbye message.")
+
+    def check_unknown(self, output):
+        if "Unknown input! Please enter a valid one." in output:
+            return "3"
+        return CheckResult.wrong("Your program couldn't process unknown input.")
+
+    def check(self, reply: str, attach: Any) -> CheckResult:
+        if "Goodbye!" in reply:
+            return CheckResult.correct()
+
+        return CheckResult.wrong("Your program didn't print a correct goodbye message.")
 
 
 if __name__ == '__main__':
---
+++ Text-Based-Adventure-Game/stage1/txt-based-adv-game/game.py
@@ -0,0 +1,50 @@
+from termcolor import cprint, colored
+
+
+def warning_unknown_input():
+    cprint("Unknown input! Please enter a valid one.", "red")
+
+
+class Menu:
+    options = """
+    
+1- Press key '1' or type 'start' to start a new game
+2- Press key '2' or type 'load' to load your progress
+3- Press key '3' or type 'quit' to quit the game"""
+
+    def __init__(self, user_input):
+        self.user_input = user_input
+
+    @staticmethod
+    def welcome():
+        title = "Journey to Mount Qaf"
+        print(colored(f"\n***Welcome to the {title}***", "yellow", attrs=["bold"]),
+              colored(Menu.options, "magenta", attrs=["bold"]))
+
+    def new_game(self):
+        cprint("Starting a new game...", "blue")
+
+    def load_game(self):
+        cprint("Loading your progress...", "blue")
+
+
+game_menu = Menu(None)
+
+Menu.welcome()
+
+while True:
+
+    game_menu.user_input = input()
+
+    if game_menu.user_input == "1" or game_menu.user_input.lower() == "start":
+        game_menu.new_game()
+
+    elif game_menu.user_input == "2" or game_menu.user_input.lower() == "load":
+        game_menu.load_game()
+
+    elif game_menu.user_input == "3" or game_menu.user_input.lower() == "quit":
+        cprint("Goodbye!", "blue")
+        break
+
+    else:
+        warning_unknown_input()
