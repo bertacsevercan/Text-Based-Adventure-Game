eaf76f5e61664c01e3cb9a6d0e6bfbd330a6f653
bertacsevercan <bertac.severcan@gmail.com>
2021-03-12 01:53:49 +0300

    remove comment

--- Text-Based-Adventure-Game/stage2/tests.py
+++ Text-Based-Adventure-Game/stage2/tests.py
@@ -262,11 +262,6 @@ class TextBasedAdventureGameTest(StageTest):
             return CheckResult.correct()
         return CheckResult.wrong(feedback)
 
-    # def check_go_back(self, output):
-    #     if "going back to menu" in output.lower():
-    #         return CheckResult.correct()
-    #     return CheckResult.wrong(
-    #         "You didn't output the correct message when going back to menu. Make sure the output contains 'going back to menu'")
 
     # def generate(self) -> [TestCase]:
     #     return [
@@ -285,26 +280,8 @@ class TextBasedAdventureGameTest(StageTest):
     #         TestCase(stdin="quIt")
     #     ]
     #
-    # def check_welcome(self, output):
-    #     if "welcome to" not in output.lower() and "***" not in output:
-    #         return CheckResult.wrong("You didn't output a correct welcome message!")
-    #     return CheckResult.correct()
-    #
-    # def check_start_load(self, output):
-    #     if "starting a new game" in output.lower() or "no save data found" in output.lower():
-    #         return CheckResult.correct()
-    #     return CheckResult.wrong("Your program didn't output correct message.")
-    #
-    # def check_unknown(self, output):
-    #     if "unknown input! please enter a valid one" in output.lower():
-    #         return "3"
-    #     return CheckResult.wrong("Your program couldn't process unknown input.")
-    #
-    # def check_username(self, output):
-    #     if "enter a username" not in output.lower() and "/b" not in output.lower():
-    #         return CheckResult.wrong("You didn't ask for the username and didn't give the option to go back.")
-    #     return self.username
-    #
+
+
     # def check_game_state(self, output):
     #     states = [self.name, self.species, self.gender, self.snack, self.weapon, self.tool, self.difficulty]
     #     have_state = all([state in output.lower() for state in states])
@@ -314,15 +291,7 @@ class TextBasedAdventureGameTest(StageTest):
     #         return CheckResult.wrong("You didn't output the correct game state.")
     #     return CheckResult.correct()
     #
-    # def check_go_back(self, output):
-    #     if "going back to menu" not in output.lower():
-    #         CheckResult.wrong("You didn't output the correct message when going back to menu.")
-    #     return "3"
-    #
-    # def check(self, reply: str, attach: Any) -> CheckResult:
-    #     if "goodbye!" in reply.lower():
-    #         return CheckResult.correct()
-    #     return CheckResult.wrong("Your program didn't print a correct goodbye message.")
+
 
 
 if __name__ == '__main__':
